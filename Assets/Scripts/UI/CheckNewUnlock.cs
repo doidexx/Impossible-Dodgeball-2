b8ebfeb93a5b66d3d5e1b2ce378ddeade8f515fe
Shany Perez <37599243+doidexx@users.noreply.github.com>
2020-12-28 22:50:20 -0400

    .

---
+++ Assets/Scripts/UI/CheckNewUnlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckNewUnlock : MonoBehaviour
+{
+    public SkinButton[] skinButtons = null;
+    public List<Sprite> newlyUnlocks = null;
+    public Transform content = null;
+
+    private void Start()
+    {
+        CheckSkins();
+        foreach (var image in newlyUnlocks)
+        {
+            Instantiate(image, content);
+        }
+    }
+
+    private void CheckSkins()
+    {
+        foreach (var button in skinButtons)
+        {
+            button.CheckUnlockable(gameObject);
+        }
+    }
+}
--- Assets/Scripts/UI/SkinButton.cs
+++ Assets/Scripts/UI/SkinButton.cs
@@ -31,12 +31,12 @@ public class SkinButton : MonoBehaviour
         holder = FindObjectOfType<DataHolder>();
         CheckLastActiveSkin();
 
-        CheckUnlockable();
+        CheckUnlockable(gameObject);
         _lock.gameObject.SetActive(!unlocked);
         scoreRequirementText += scoreRequirement;
     }
 
-    private void CheckUnlockable()
+    public void CheckUnlockable(GameObject obj)
     {
         unlocked = holder.IsInList(material.GetInstanceID());
         var scoreReached = holder.highScore >= scoreRequirement;
@@ -44,6 +44,8 @@ public class SkinButton : MonoBehaviour
             return;
         holder.AddToOwnedSkins(material.GetInstanceID());
         unlocked = true;
+        if (obj != gameObject)
+            obj.GetComponent<CheckNewUnlock>().newlyUnlocks.Add(sprite);
     }
 
     private void CheckLastActiveSkin()
